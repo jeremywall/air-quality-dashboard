cb0a305d56e5d2a1e34fb9c9c6f21583d44f4a60
Jeremy Wall <jeremy.wall@gmail.com>
2020-08-26 14:00:56 -0700

    chart attempt 1

--- index.php
+++ index.php
@@ -59,7 +59,11 @@ if ($aqSensor != null) {
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-
+<style type="text/css">
+#api-container {
+    height: 400px;
+}
+</style>
 <title><?php echo( $siteTitle); ?></title>
 </head>
 <body>
@@ -67,9 +71,94 @@ if ($aqSensor != null) {
 <p>Last Updated: <?php echo($recordDateTime->format("M j, Y \a\\t h:i A e")); ?></p>
 <p>PM 2.5 Concentration = <?php echo($pm25); ?></p>
 <p>PM 2.5 AQI = <?php echo($pm25AqiValue); ?> <?php echo($pm25AqiDesc); ?></p>
+<div id="aqi-container"></div>
 <?php } ?>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+<script src="https://code.highcharts.com/highcharts.js"></script>
+<script>
+    Highcharts.chart('api-container', {
+
+        chart: {
+            type: 'gauge',
+            plotBackgroundColor: null,
+            plotBackgroundImage: null,
+            plotBorderWidth: 0,
+            plotShadow: false
+        },
+
+        title: {
+            text: 'Current PM 2.5 AQI'
+        },
+
+        pane: {
+            startAngle: -135,
+            endAngle: 135,
+            background: [ ]
+        },
+
+        // the value axis
+        yAxis: {
+            min: 0,
+            max: 500,
+
+            minorTickInterval: 'auto',
+            minorTickWidth: 1,
+            minorTickLength: 10,
+            minorTickPosition: 'inside',
+            minorTickColor: '#000',
+
+            tickPixelInterval: 30,
+            tickWidth: 2,
+            tickPosition: 'inside',
+            tickLength: 10,
+            tickColor: '#000',
+            labels: {
+                step: 2,
+                rotation: 'auto',
+                distance: 5
+            },
+            plotBands: [{
+                innerRadius: 100,
+                from: 0,
+                to: 50,
+                color: '#0bab8b' // green
+            }, {
+
+                innerRadius: 100,
+                from: 51,
+                to: 100,
+                color: '#ede400' // yellow
+            }, {
+                innerRadius: 100,
+                from: 101,
+                to: 150,
+                color: '#ed8b00' // orange
+            }, {
+                innerRadius: 100,
+                from: 151,
+                to: 200,
+                color: '#bd0000' // red
+            }, {
+                innerRadius: 100,
+                from: 201,
+                to: 300,
+                color: '#a7005b' // purple
+            }, {
+                innerRadius: 100,
+                from: 301,
+                to: 500,
+                color: '#5f0000' // maroon
+            }]
+        },
+
+        series: [{
+            name: 'AQI',
+            data: [<?php echo($pm25AqiValue); ?>]
+        }]
+
+    });
+</script>
 </body>
 </html>
