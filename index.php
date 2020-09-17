ed5cf7657a99555e29c37b3d12d4cde7f3919f19
Jeremy Wall <jeremywall@users.noreply.github.com>
2020-09-17 11:16:31 -0700

    Update index.php

--- index.php
+++ index.php
@@ -144,8 +144,16 @@ if ($aqSensor != null) {
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-<script src="https://code.highcharts.com/highcharts.js"></script>
-<script src="https://code.highcharts.com/highcharts-more.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment-timezone/0.5.31/moment-timezone-with-data.min.js" integrity="sha512-HZcf3uHWA+Y2P5KNv+F/xa87/flKVP92kUTe/KXjU8URPshczF1Dx+cL5bw0VBGhmqWAK0UbhcqxBbyiNtAnWQ==" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highcharts/7.2.2/highcharts.js" integrity="sha512-sYAnmpNKTJz8xm4GBRk/dblXZC8k8MLJKmGQovDbPgPrZTtTwGOw2QitwA39oT/49SwQ1JUN7ttI6rmjxNfxLw==" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highcharts/7.2.2/highcharts-more.min.js" integrity="sha512-KC91jt+QSZbKBHpQGH4M78Mb5xFfhU1BkFekDHS4QlObVQ7XkySXXDl2wjKXmvOskNxN/SpU76ypigb880Eugg==" crossorigin="anonymous"></script>
+<script>
+    Highcharts.setOptions({
+        time: {
+            timezone: '<?php echo(getenv("TIMEZONE")); ?>'
+        }
+    });
+</script>
 <?php if ($currentData != null) { ?>
 <script>
     Highcharts.chart('aqi-current-gauge-container', {
@@ -279,7 +287,12 @@ if ($aqSensor != null) {
             }]
         },
         xAxis: {
-            type: 'datetime'
+            type: 'datetime',
+            labels: {
+               formatter: function() {
+                   return this;
+               }
+            }
         },
         series: [{
             name: 'Avg',
