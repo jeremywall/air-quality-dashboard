fa6c56a45c4a4ecec1effdf062395f4d25ac019e
Jeremy Wall <jeremywall@users.noreply.github.com>
2020-09-17 10:59:27 -0700

    Update index.php

--- index.php
+++ index.php
@@ -129,6 +129,9 @@ if ($aqSensor != null) {
 #aqi-trend-chart-container {
     height: 400px;
 }
+#bottom-padding-container {
+    height: 200px;
+}
 </style>
 <title><?php echo( $siteTitle); ?></title>
 </head>
@@ -136,7 +139,8 @@ if ($aqSensor != null) {
 
 <div id="aqi-current-gauge-container"></div>
 <div id="aqi-trend-chart-container"></div>
-
+<div id="bottom-padding-container"></div>
+    
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
