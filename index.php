39227aa8b081f880fe692f04ba3a4547d7922723
Jeremy Wall <jeremywall@users.noreply.github.com>
2020-09-17 00:03:20 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 $siteTitle = getenv("SITE_TITLE");
 
+// get current data
 $parameters = array(
     "api-key" => getenv("WEATHERLINK_V2_API_KEY"),
     "station-id" => getenv("STATION_ID"),
@@ -34,23 +35,79 @@ foreach ($sensors as $sensor) {
     }
 }
 
-$recordTs = null;
-$pm25 = null;
-$pm25AqiValue = null;
-$pm25AqiDesc = null;
+$currentData = null;
 if ($aqSensor != null) {
+    $currentData = new stdClass();
+
     $record = $aqSensor->data[0];
 
-    $recordTs = $record->ts;
-
     $recordDateTime = new \DateTime();
-    $recordDateTime->setTimestamp($recordTs);
+    $recordDateTime->setTimestamp($record->ts);
     $recordDateTime->setTimezone(new \DateTimeZone(getenv("TIMEZONE")));
+    
+    $currentData->dateTime = $recordDateTime;
 
-    $pm25 = $record->pm_2p5;
-    $pm25AqiValue = round($record->aqi_val, 1);
-    $pm25AqiDesc = $record->aqi_desc;
+    $currentData->pm25 = $record->pm_2p5;
+    $currentData->pm25AqiValue = round($record->aqi_val, 1);
+    $currentData->pm25AqiDesc = $record->aqi_desc;
 }
+
+// get trend data
+$parameters = array(
+    "api-key" => getenv("WEATHERLINK_V2_API_KEY"),
+    "station-id" => getenv("STATION_ID"),
+    "start-timestamp" => time() - (3600 * 12),
+    "end-timestamp" => time(),
+    "t" => time()
+);
+
+ksort($parameters);
+
+$stringToHash = "";
+foreach ($parameters as $key => $value) {
+    $stringToHash = $stringToHash . $key . $value;
+}
+
+$apiSignature = hash_hmac("sha256", $stringToHash, getenv("WEATHERLINK_V2_API_SECRET"));
+
+$json = file_get_contents(getenv("WEATHERLINK_V2_API_BASE_URL") .
+    "/historic/" . $parameters["station-id"] .
+    "?api-key=" . $parameters["api-key"] .
+    "&api-signature=$apiSignature" .
+    "&start-timestamp=" . $parameters["start-timestamp"] .
+    "&end-timestamp=" . $parameters["end-timestamp"] .
+    "&t=" . $parameters["t"]
+);
+
+$data = json_decode($json);
+
+$sensors = $data->sensors;
+$aqSensor = null;
+$sensorId = (int) getenv("SENSOR_ID");
+foreach ($sensors as $sensor) {
+    if ($sensor->lsid == $sensorId) {
+        $aqSensor = $sensor;
+    }
+}
+
+$trendData = null;
+if ($aqSensor != null) {
+    $trendData = new stdClass();
+
+    $record = $aqSensor->data[0];
+
+//     $recordDateTime = new \DateTime();
+//     $recordDateTime->setTimestamp($record->ts);
+//     $recordDateTime->setTimezone(new \DateTimeZone(getenv("TIMEZONE")));
+    
+//     $currentData->dateTime = $recordDateTime;
+
+//     $currentData->pm25 = $record->pm_2p5;
+//     $currentData->pm25AqiValue = round($record->aqi_val, 1);
+//     $currentData->pm25AqiDesc = $record->aqi_desc;
+}
+
+
 
 ?><!doctype html>
 <html lang="en">
@@ -60,7 +117,10 @@ if ($aqSensor != null) {
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 <style type="text/css">
-#api-container {
+#aqi-current-gauge-container {
+    height: 400px;
+}
+#aqi-trend-chart-container {
     height: 400px;
 }
 </style>
@@ -68,17 +128,17 @@ if ($aqSensor != null) {
 </head>
 <body>
 <?php if ($aqSensor != null) { ?>
-<div id="aqi-container"></div>
+<div id="aqi-current-gauge-container"></div>
+<div id="aqi-trend-chart-container"></div>
 <?php } ?>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 <script src="https://code.highcharts.com/highcharts.js"></script>
 <script src="https://code.highcharts.com/highcharts-more.js"></script>
-<?php if ($aqSensor != null) { ?>
+<?php if ($currentData != null) { ?>
 <script>
-    Highcharts.chart('aqi-container', {
-
+    Highcharts.chart('aqi-current-gauge-container', {
         chart: {
             type: 'gauge',
             plotBackgroundColor: null,
@@ -86,32 +146,26 @@ if ($aqSensor != null) {
             plotBorderWidth: 0,
             plotShadow: false
         },
-
         title: {
-            text: 'Last Updated: <?php echo($recordDateTime->format("M j, Y @ h:i A")); ?><br>Current PM 2.5 AQI<br><?php echo($pm25AqiDesc); ?>'
+            text: 'Last Updated: <?php echo($currentData->dateTime->format("M j, Y @ h:i A")); ?><br>Current PM 2.5 AQI<br><?php echo($currentData->pm25AqiDesc); ?>'
         },
-
         credits: {
             enabled: false
         },
-
         pane: {
             startAngle: -135,
             endAngle: 135,
             background: [ ]
         },
-
         // the value axis
         yAxis: {
             min: 0,
             max: 500,
-
             minorTickInterval: 'auto',
             minorTickWidth: 1,
             minorTickLength: 10,
             minorTickPosition: 'inside',
             minorTickColor: '#000',
-
             tickPixelInterval: 30,
             tickWidth: 2,
             tickPosition: 'inside',
@@ -128,7 +182,6 @@ if ($aqSensor != null) {
                 to: 50,
                 color: '#0bab8b' // green
             }, {
-
                 innerRadius: 50,
                 from: 50,
                 to: 100,
@@ -155,12 +208,10 @@ if ($aqSensor != null) {
                 color: '#5f0000' // maroon
             }]
         },
-
         series: [{
             name: 'AQI',
-            data: [<?php echo($pm25AqiValue); ?>]
+            data: [<?php echo($currentData->pm25AqiValue); ?>]
         }]
-
     });
 </script>
 <?php } ?>
