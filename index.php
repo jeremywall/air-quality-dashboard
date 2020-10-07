33f3bcf4c4487135ad97dfb88a4a53efbab2a77b
Jeremy Wall <jeremywall@users.noreply.github.com>
2020-10-07 10:25:35 -0700

    Update index.php

--- index.php
+++ index.php
@@ -118,6 +118,7 @@ if ($aqSensor != null) {
 ?><!doctype html>
 <html lang="en">
 <head>
+<meta http-equiv="refresh" content="60">
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
