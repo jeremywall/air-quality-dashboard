4fd4214c137fdf0513fb940a6ef92f307459a230
Jeremy Wall <jeremywall@users.noreply.github.com>
2021-09-14 11:48:33 -0700

    test

--- src/functions/initialize/index.js
+++ src/functions/initialize/index.js
@@ -2,8 +2,8 @@ const _ = require("lodash");
 
 exports.handler = async function(event, context) {
     let data = {
-      siteTitle: process.env.SITE_TITLE,
-      timeZone: process.env.TIMEZONE
+      title: process.env.SITE_TITLE,
+      timezone: process.env.TIMEZONE
     };
     return {
         statusCode: 200,
--- src/functions/refresh-data/index.js
+++ src/functions/refresh-data/index.js
@@ -4,12 +4,19 @@ const crypto = require("crypto");
 exports.handler = async function(event, context) {
     const data = {
         current: {
-            
+            timestamp: null, // number
+            pm25: null, // number
+            pm25_aqi_value: null, // number
+            pm25_aqi_desc: null // string
         },
         historic: {
-
+            data: []    // { timestamp: number, pm25_aqi_value: number }
         }
     };
+
+    const response = await fetch("http://httpbin.org/get");
+    const json = await response.json();
+    data.extra = json;
 
     return {
         statusCode: 200,
